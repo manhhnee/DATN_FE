3092a16a2d5571eda619ec8e5cbd876f82828609
Manh <manhddt2508@gmail.com>
2024-06-17 13:56:15 +0700

    route

--- src/axiosConfig/axiosConfig.jsx
+++ src/axiosConfig/axiosConfig.jsx
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3000/api/v1',
+  baseURL: 'https://2hm-ai-store.click/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
