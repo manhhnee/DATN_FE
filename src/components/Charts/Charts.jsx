c85ef2b531fb388b22c7391b1723df9d6fb53a12
Manh <manhddt2508@gmail.com>
2024-06-25 11:08:06 +0700

    fix

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -96,7 +96,7 @@ const Charts = () => {
     <div className="sm:px-7.5 col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-5">
       <div className="mb-3 justify-between gap-4 sm:flex">
         <div>
-          <h5 className="text-xl font-semibold text-black dark:text-white">Top customer</h5>
+          <h5 className="text-xl font-semibold text-black dark:text-white">Top staff</h5>
         </div>
       </div>
       <div className="mb-2">
